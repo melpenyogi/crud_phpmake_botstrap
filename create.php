c4e56a31bdf974383e2554df7d774554e0c87250
melpenyogi <melpenyogi2019@gmail.com>
2024-06-29 17:19:31 +0900

    CRUD: Learn php make bootstrap

---
+++ create.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form Pendaftaran Peserta</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <?php
+    //Include file koneksi, untuk koneksikan ke database
+    include "koneksi.php";
+
+    //Fungsi untuk mencegah inputan karakter yang tidak sesuai
+    function input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    //Cek apakah ada kiriman form dari method post
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $nama=input($_POST["nama"]);
+        $sekolah=input($_POST["sekolah"]);
+        $jurusan=input($_POST["jurusan"]);
+        $no_hp=input($_POST["no_hp"]);
+        $alamat=input($_POST["alamat"]);
+
+        //Query input menginput data kedalam tabel anggota
+        $sql="insert into peserta (nama,sekolah,jurusan,no_hp,alamat) values
+		('$nama','$sekolah','$jurusan','$no_hp','$alamat')";
+
+        //Mengeksekusi/menjalankan query diatas
+        $hasil=mysqli_query($kon,$sql);
+
+        //Kondisi apakah berhasil atau tidak dalam mengeksekusi query diatas
+        if ($hasil) {
+            header("Location:index.php");
+        }
+        else {
+            echo "<div class='alert alert-danger'> Data Gagal disimpan.</div>";
+
+        }
+
+    }
+    ?>
+    <h2>Input Data</h2>
+
+
+    <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
+        <div class="form-group">
+            <label>Nama:</label>
+            <input type="text" name="nama" class="form-control" placeholder="Masukan Nama" required />
+
+        </div>
+        <div class="form-group">
+            <label>Sekolah:</label>
+            <input type="text" name="sekolah" class="form-control" placeholder="Masukan Nama Sekolah" required/>
+        </div>
+       <div class="form-group">
+            <label>Jurusan :</label>
+            <input type="text" name="jurusan" class="form-control" placeholder="Masukan Jurusan" required/>
+        </div>
+                </p>
+        <div class="form-group">
+            <label>No HP:</label>
+            <input type="text" name="no_hp" class="form-control" placeholder="Masukan No HP" required/>
+        </div>
+        <div class="form-group">
+            <label>Alamat:</label>
+            <textarea name="alamat" class="form-control" rows="5"placeholder="Masukan Alamat" required></textarea>
+        </div>       
+
+        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+</body>
+</html>
---
+++ db/learn_crud.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 29, 2024 at 10:16 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `learn_crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peserta`
+--
+
+CREATE TABLE `peserta` (
+  `id_peserta` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `sekolah` varchar(50) NOT NULL,
+  `jurusan` varchar(50) NOT NULL,
+  `no_hp` char(13) NOT NULL,
+  `alamat` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `peserta`
+--
+
+INSERT INTO `peserta` (`id_peserta`, `nama`, `sekolah`, `jurusan`, `no_hp`, `alamat`) VALUES
+(1, 'Opnis Degei', 'SMK Karel Paniai', 'Perkantoran', '081212924706', 'Waiyamo'),
+(3, 'Petrus Degei', 'SMK Karel Paniai', 'Perkantoran', '081212924709', 'Kampung Eka'),
+(4, 'Melpen Yogi', 'SMK Harapan Timika', 'Teknik Informatika', '081212924706', 'Nawaripi');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `peserta`
+--
+ALTER TABLE `peserta`
+  ADD PRIMARY KEY (`id_peserta`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `peserta`
+--
+ALTER TABLE `peserta`
+  MODIFY `id_peserta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
